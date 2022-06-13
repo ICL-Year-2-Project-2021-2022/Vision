2baef7eef2995670c4a59867f7beb9d8b94162cb
Marco Chan <marco.chan616@gmail.com>
2022-06-13 22:01:44 +0100

    tested get_exp

--- C_Conversion/testing.c
+++ C_Conversion/testing.c
@@ -69,28 +69,32 @@ int test_transpose(){
 }
 
 
-struct Land{
+
+
+
+struct Land{ //diff from kalman
     float x;
     float y;
     char *color;
 };
 
-struct Land_List{
+struct Landmarks{
     size_t size;
-    struct Land items[10];
+    struct Land item[6];
 };
 
-struct Seen_List{
+struct Seen_Land_List{
     size_t size;
-    struct Land items[10];
+    struct Land item[6];
+    //red 0, blue 1, green 2, yellow 3, black 4, pink 5  
 };
 
-void find_color(struct Seen_List seen_list, struct Land_List land_list){
+void find_color(struct Seen_Land_List seen_list, struct Landmarks land_list){
     int i,j=0;
     for (i=0; i<land_list.size; i++){
         for(j=0; j<seen_list.size; j++){
-            if(land_list.items[i].color == seen_list.items[j].color){
-                printf("%s", land_list.items[i].color);
+            if(land_list.item[i].color == seen_list.item[j].color){
+                printf("%s", land_list.item[i].color);
             }
         }
     }
@@ -104,28 +108,66 @@ void test_find_color(){
     l2.x=2;
     l2.y=4;
     l2.color= "blue";
-    struct Land_List list1;
+    struct Landmarks list1;
     list1.size=0;
-    list1.items[list1.size]=l1;
+    list1.item[list1.size]=l1;
     list1.size++;
-    list1.items[list1.size]=l2;
+    list1.item[list1.size]=l2;
     list1.size++;
 
-    struct Seen_List seen_list;
+    struct Seen_Land_List seen_list;
     seen_list.size=0;
-    seen_list.items[seen_list.size]=l2;
+    seen_list.item[seen_list.size]=l2;
     seen_list.size++;
     find_color(seen_list,list1);
     //should display blue
 }
 
+enum colorcode{red, blue, green, yellow, black, pink};
 
+// void test_enum(){
+//     enum colorcode color_num;
+//     color_num = str2enum("red"); //doesnt work, can use hash map but lazy to impelment
+//     //just use if statements
+//     printf("%d", color_num);
+// }
+
+
+
+void get_exp(float pred_state[5][1], struct Seen_Land_List seen_list){
+    float x_coor, y_coor;
+    x_coor = seen_list.item[0].x;
+    y_coor = seen_list.item[0].y;
+    //obtain expected observation
+    float delta[2];//distance of landmark to robot
+    delta[0] = x_coor - pred_state[0][0];
+    delta[1] = y_coor - pred_state[1][0];
+    float q = dotProduct(delta, delta, 2);
+    float exp_dis_ang[2]; //z
+    exp_dis_ang[0] = sqrt(q); 
+    exp_dis_ang[1] = atan2(delta[1],delta[2]) - pred_state[2][0];
+    printf ("q: %f\nexp: %f %f", q, exp_dis_ang[0], exp_dis_ang[1]);
+}
+
+void test_get_exp(){
+    struct Land l1;
+    l1.x=1;
+    l1.y=2;
+    l1.color= "red";
+    struct Seen_Land_List seen_list;
+    seen_list.size=0;
+    seen_list.item[seen_list.size]=l1;
+    float pred_state[5][1] = {{2}, {4}, {6}, {3}, {4.5}};
+    get_exp(pred_state, seen_list);
+    
+}
 int main(){
     //test_row_switch();
     //test_inverse1();
     //test_inverse2();
     //test_matrix_multi();
-    test_find_color();
+    //test_find_color();
+    test_get_exp();
     
     
     return 0;
